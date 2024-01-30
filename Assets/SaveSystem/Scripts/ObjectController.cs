bf57d86075eb52054d3a230bc2eaa06fefacb85f
kayra <157609274+ibrahimKayraDural@users.noreply.github.com>
2024-01-30 17:20:05 +0300

    playable balls

---
+++ Assets/SaveSystem/Scripts/ObjectController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class ObjectController : MonoBehaviour
+{
+    public Color ObjectColor => _colors[_colorIndex];
+
+    [SerializeField, Min(0.1f)] float _MaxSize = 3;
+    [SerializeField, Min(0.1f)] float _MinSize = .1f;
+
+    Rigidbody2D _rb;
+    SpriteRenderer _sr;
+    Color[] _colors = new Color[]
+    {   Color.white, Color.gray, Color.black, Color.red, Color.yellow,
+        Color.green, Color.cyan, Color.blue, Color.magenta };
+
+    int _colorIndex = 0;
+
+    void Awake()
+    {
+        if (_MaxSize < _MinSize) _MaxSize = _MinSize;
+
+        _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+
+        RefreshColor();
+    }
+
+    public void MoveTo(Vector2 target)
+    {
+        _rb.MovePosition(target);
+    }
+    public void SetSize(float setTo)
+    {
+        transform.localScale = Vector2.one * Mathf.Clamp(setTo, _MinSize, _MaxSize);
+    }
+    public void AddToSize(float addition) => SetSize(transform.localScale.x + addition);
+    public void SetNextColor()
+    {
+        _colorIndex = (_colorIndex + 1) % _colors.Length;
+        RefreshColor();
+    }
+    public void SetPreviousColor()
+    {
+        _colorIndex = _colorIndex > 0 ? _colorIndex - 1 : _colors.Length - 1;
+        RefreshColor();
+    }
+    void RefreshColor() => _sr.color = _colors[_colorIndex];
+}
---
+++ Assets/SaveSystem/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaveSystem
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] GameObject _ObjectPrefab;
+        [SerializeField] int _MaxObjectCount = 100;
+        [SerializeField] float _ScaleSpeed = 100;
+
+        List<ObjectController> _objectsInScene = new List<ObjectController>();
+        ObjectController _heldObject = null;
+
+        void Start()
+        {
+            ObjectController[] objects = FindObjectsOfType<ObjectController>(true);
+
+            foreach (ObjectController oc in objects)
+            {
+                _objectsInScene.Add(oc);
+            }
+        }
+
+        void Update()
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            ObjectController hoverOverController = null;
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, 100, 1 << 6);
+            if (hit.collider != null && hit.collider.gameObject.TryGetComponent(out ObjectController oc))
+            {
+                hoverOverController = oc;
+            }
+
+            if (Input.GetMouseButtonDown(0) && _heldObject == null)
+            {
+                _heldObject = hoverOverController;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                _heldObject = null;
+            }
+            else if ((Input.GetMouseButtonDown(1) || (Input.GetMouseButton(1) && Input.GetKey(KeyCode.LeftControl)))
+                && _objectsInScene.Count < _MaxObjectCount)
+            {
+                if (_ObjectPrefab.TryGetComponent(out ObjectController _))
+                {
+                    _objectsInScene.Add(Instantiate(_ObjectPrefab, mousePos, Quaternion.identity)
+                        .GetComponent<ObjectController>());
+                }
+            }
+
+            if (_heldObject != null)
+            {
+                _heldObject.MoveTo(mousePos);
+                SetScaleByMouseWheel(_heldObject);
+                if (Input.GetMouseButtonDown(2) || Input.GetKeyDown(KeyCode.E)) _heldObject.SetNextColor();
+                else if (Input.GetKeyDown(KeyCode.Q)) _heldObject.SetPreviousColor();
+            }
+            else if (hoverOverController != null)
+            {
+                SetScaleByMouseWheel(hoverOverController);
+                if (Input.GetMouseButtonDown(2) || Input.GetKeyDown(KeyCode.E)) hoverOverController.SetNextColor();
+                else if (Input.GetKeyDown(KeyCode.Q)) hoverOverController.SetPreviousColor();
+            }
+        }
+
+        void SetScaleByMouseWheel(ObjectController obj)
+        {
+            if (obj == null) return;
+
+            obj.AddToSize(Input.GetAxisRaw("Mouse ScrollWheel") * Time.deltaTime * _ScaleSpeed);
+        }
+    }
+}
